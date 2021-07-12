9766e87efb0e80534f0ea164d778d776d1b29e85
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-12 19:46:01 +0530

    add Decorator to validate and trim the token

---
+++ src/Decorators/Validate.ts
@@ -0,0 +1,22 @@
+import { GithubClient } from '../Github'
+
+const Validate = () => {
+    return (target: GithubClient, key: keyof GithubClient): void => {
+        let value = target[key]
+        if (!value) throw new Error(`Empty Token Recived`)
+        const get = () => value
+
+        const set = (token: string) => {
+            value = token.trim()
+        }
+
+        Object.defineProperty(target, key, {
+            get,
+            set,
+            enumerable: true,
+            configurable: true
+        })
+    }
+}
+
+export default Validate
