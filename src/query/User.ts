1bac14d882164ed9b5b8db6a03c49ee532470e3e
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-12 23:08:52 +0530

    add Interface for User Stats

--- src/query/User.ts
+++ src/query/User.ts
@@ -28,3 +28,32 @@ export default (): string => `query userInfo($login: String!) {
         }
     }
 }`
+
+export interface IUserStats {
+    name: string
+    login: string
+    pullRequests: {
+        totalCount: number
+    }
+    issues: {
+        totalCount: number
+    }
+    followers: {
+        totalCount: number
+    }
+    contributionsCollection: {
+        totalCommitContributions: number
+        restrictedContributionsCount: number
+    }
+    repositoriesContributedTo: {
+        totalCount: number
+    }
+    repositories: {
+        totalCount: number
+        nodes: {
+            stargazers: {
+                totalCount: number
+            }
+        }
+    }
+}
