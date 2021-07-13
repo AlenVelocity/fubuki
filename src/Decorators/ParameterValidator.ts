67026703c45097db8b10d42a1872da4aa5d571f7
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-13 12:52:01 +0530

    Update `ParameterValidator.ts`

--- src/Decorators/ParameterValidator.ts
+++ src/Decorators/ParameterValidator.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata'
 import { GithubClient } from '../GithubClient'
+import { IUserStats } from '../query/User'
 const requiredMetadataKey = Symbol('required')
 
+type Filter<T> = {
+    [K in keyof T as Exclude<K, 'BASE_URL' | 'token'>]: T[K]
+}
+
 export const Required = () => {
-    return (target: GithubClient, key: 'userStats', index: number): void => {
+    return (target: GithubClient, key: keyof Filter<GithubClient>, index: number): void => {
         const existingRequiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, key) || []
         existingRequiredParameters.push(index)
         Reflect.defineMetadata(requiredMetadataKey, existingRequiredParameters, target, key)
@@ -13,11 +18,13 @@ export const Required = () => {
 export const Validate = () => {
     return (
         target: GithubClient,
-        propertyName: 'userStats',
+        propertyName: keyof Filter<GithubClient>,
         descriptor: TypedPropertyDescriptor<typeof target[typeof propertyName]>
     ): void => {
-        const method = descriptor.value
-        descriptor.value = async function (...args: Parameters<typeof target[typeof propertyName]>) {
+        const method = descriptor.value as unknown as () => IUserStats
+        ;(descriptor.value as unknown as () => Promise<IUserStats>) = async function (
+            ...args: Parameters<typeof target[typeof propertyName]>
+        ) {
             const requiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyName)
             if (requiredParameters) {
                 for (const parameterIndex of requiredParameters) {
@@ -25,7 +32,8 @@ export const Validate = () => {
                         throw new Error('Missing required argument.')
                 }
             }
-            return method?.apply(this, args) as ReturnType<typeof target[typeof propertyName]>
+            //@ts-expect-error Cause the `@param` decorator to be applied to the wrong type.
+            return method?.apply(this, args as unknown[]) as unknown as IUserStats
         }
     }
 }
