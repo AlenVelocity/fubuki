7ab8bf61e07036d942ac6a8f2e96977bf3b52d5c
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-13 12:52:43 +0530

    Update repo query and add interface

--- src/query/Repository.ts
+++ src/query/Repository.ts
@@ -1,9 +1,5 @@
-export default (): string => `query {
-    repositoryOwner (login: $login) {
-      repositories {
-        totalCount
-      }
-      repository(name: $repository) {
+export default (): string => `query repoInfo($owner: String!, $repository: String!) {
+      repository(name: $repository, owner: $owner) {
         description
         forks {
           totalCount
@@ -35,5 +31,60 @@ export default (): string => `query {
           }
         }
       }
-    }
-  }`
+    }`
+
+// Created using Github Copilot
+export interface IRepository {
+    description: string
+    forks: RepositoryForks
+    issues: RepositoryIssues
+    stargazers: RepositoryStargazers
+    watchers: RepositoryWatchers
+    pullRequests: RepositoryPullRequests
+    labels: RepositoryLabels
+    milestones: RepositoryMilestones
+}
+
+interface RepositoryForks {
+    totalCount: number
+}
+
+interface RepositoryIssues {
+    totalCount: number
+}
+
+interface RepositoryStargazers {
+    totalCount: number
+}
+
+interface RepositoryWatchers {
+    totalCount: number
+}
+
+interface RepositoryPullRequests {
+    totalCount: number
+}
+
+interface RepositoryLabels {
+    edges: RepositoryLabelsEdge[]
+}
+
+interface RepositoryLabelsEdge {
+    node: RepositoryLabelsNode
+}
+
+interface RepositoryLabelsNode {
+    name: string
+}
+
+interface RepositoryMilestones {
+    edges: RepositoryMilestonesEdge[]
+}
+
+interface RepositoryMilestonesEdge {
+    node: RepositoryMilestonesNode
+}
+
+interface RepositoryMilestonesNode {
+    title: string
+}
