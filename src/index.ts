b49facd86c660dbd56e0cc5dd5cc0fa1cf163051
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-14 13:43:58 +0530

    Update Exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './GithubClient'
+export { IUser } from './query/User'
+export { IRepository } from './query/Repository'
+export { IContributionsCalender } from './query/ContributionCalender'
